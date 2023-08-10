0f77879b572f1b9baeb4afdec2e9cc62cb80b13f
Анатолий Савин <beningtonchess@yandex.ru>
2023-08-10 16:49:27 +0300

    initial commit

---
+++ parsing_hh.py
@@ -0,0 +1,111 @@
+'''
+<main class="vacancy-serp-content"><div data-qa="vacancy-serp__results" id="a11y-main-content"><div class="bloko-v-spacing bloko-v-spacing_base-6 bloko-v-spacing_xs-0">
+все вакансии на странице
+'''
+
+'''
+<div data-qa="vacancy-serp__vacancy-address" class="bloko-text">Москва</div>
+<div class="vacancy-serp-item__info"><div class="bloko-v-spacing-container bloko-v-spacing-container_base-2"><div class="bloko-text"><div class="vacancy-serp-item__meta-info-company"><a data-qa="vacancy-serp__vacancy-employer" class="bloko-link bloko-link_kind-tertiary" href="/employer/9468959?hhtmFrom=vacancy_search_list">Alliesverse</a></div><div class="icons-container--GiWeIyFGCiDhxBJFS19n"><div class="bloko-h-spacing-container bloko-h-spacing-container_base-1"><a class="bloko-link" target="_blank" href="https://feedback.hh.ru/article/details/id/5951"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="bloko-icon bloko-icon_initial-gray-50"><g clip-path="url(#clip0_368_29)"><path d="M15.5 8C15.5 12.1421 12.1421 15.5 8 15.5C3.85786 15.5 0.5 12.1421 0.5 8C0.5 3.85786 3.85786 0.5 8 0.5C12.1421 0.5 15.5 3.85786 15.5 8Z" stroke="var(--bloko-icon-color, var(--bloko-icon-color-default))" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4.99992 8L6.99992 10L10.9999 6" stroke="var(--bloko-icon-color, var(--bloko-icon-color-default))" stroke-linecap="round" stroke-linejoin="round"></path></g><defs><clipPath id="clip0_368_29"><rect width="16" height="16" fill="white"></rect></clipPath></defs></svg></a><div><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="bloko-icon bloko-icon_initial-gray-50"><path fill-rule="evenodd" clip-rule="evenodd" d="M15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.61526 4.48267C4.33912 4.48267 4.11526 4.70652 4.11526 4.98267C4.11526 5.25881 4.33912 5.48267 4.61526 5.48267H5.01865V10.5173L4.61533 10.5173C4.33918 10.5173 4.1153 10.7411 4.11526 11.0172C4.11523 11.2934 4.33906 11.5173 4.6152 11.5173L5.50835 11.5174L5.51865 11.5175L5.52883 11.5174L6.35968 11.5175C6.63582 11.5175 6.8597 11.2937 6.85974 11.0176C6.85977 10.7414 6.63594 10.5175 6.3598 10.5175L6.01865 10.5175V5.48267H6.35974C6.63588 5.48267 6.85974 5.25881 6.85974 4.98267C6.85974 4.70652 6.63588 4.48267 6.35974 4.48267H4.61526ZM9.49883 11.0173V5.48288H7.77102C7.49488 5.48288 7.27102 5.25902 7.27102 4.98288C7.27102 4.70674 7.49488 4.48288 7.77102 4.48288H9.98409L9.99883 4.48267L10.0136 4.48288H12.2266C12.5028 4.48288 12.7266 4.70674 12.7266 4.98288C12.7266 5.25902 12.5028 5.48288 12.2266 5.48288H10.4988V11.0173C10.4988 11.2934 10.275 11.5173 9.99883 11.5173C9.72268 11.5173 9.49883 11.2934 9.49883 11.0173Z" fill="var(--bloko-icon-color, var(--bloko-icon-color-default))"></path></svg></div></div></div></div></div><div data-qa="vacancy-serp__vacancy-address" class="bloko-text">Москва</div><div class="bloko-v-spacing bloko-v-spacing_base-3"></div></div>
+город вакансии
+<div class="wrapper--FVo3cUofDgv3zkHBdMP1">
+'''
+
+'''
+<a data-qa="vacancy-serp__vacancy-employer" class="bloko-link bloko-link_kind-tertiary" href="/employer/9468959?hhtmFrom=vacancy_search_list">Alliesverse</a>
+название компании
+'''
+'''
+<span data-qa="vacancy-serp__vacancy-compensation" class="bloko-header-section-2">250 000 – 350 000 <!-- -->₽</span>
+зарплата
+
+'''
+
+'''
+<h3 data-qa="bloko-header-3" class="bloko-header-section-3"><span data-page-analytics-event="vacancy_search_suitable_item"><a class="serp-item__title" data-qa="serp-item__title" target="_blank" href="https://adsrv.hh.ru/click?b=530538&amp;c=5&amp;place=35&amp;meta=osjzUBESCZnS1VKbPz8bZ4kzQqZcvoUAhvUMbVyNPDRuMFl1po4Q0Xmg1cjeXgsZjLmSonCYW2zCprtVhD5R30lRBGQDjE8ZN5rbLlkdgYQ3_qLS3ec7pE0nM5mQU3Gb2033vcLAflqDTv0dDwAOYUhJ7y_zFpwepaxQAXBTWnw_pZJGF0kKOKhLDwF-MJkzfHOKbJn53dzTfQFyjulBE4BpW3wGH71Qm8JFaz5b0kUiId42I51F-jVP7Dr87sgOGOQgZDA63jwPQQ19X4iOfA7-l0sJpSrYapvQmDGnjU_b9mZPEAmS1eeWYxobW7xSekJMEH6G7RrBLFp-92hgqPona880xEFKNN_enYp8dsX7VUcnuWfUSOqrP57y-KsmH7W6ig_Bb2khUasA-43fr_14l6hyXuWBCqJuEWiTanaHzEMrkngFnjzAMLA4RkEPBP1T40jlxv9SKTVt4DRiy8mt5GVNczHvOxAh-KQOpXGHsrgfqUNnASi4_H2owBeTHnuYb88nAxmFah1zXFIaCw%3D%3D&amp;clickType=link_to_vacancy">Machine Learning Engineer (ML)</a></span></h3>
+
+ссылка для перехода на страницу
+
+<div class="vacancy-serp-item__layout"><div class="vacancy-serp-item-body">
+'''
+
+'''
+<div class="g-user-content" data-qa="vacancy-description"><p><strong><span>Задачи:</span>
+тело вакансии, описание
+'''
+
+import requests
+from bs4 import BeautifulSoup
+import fake_headers
+from pprint import pprint
+import unicodedata
+import json
+
+
+headers_gen = fake_headers.Headers(browser = 'firefox', os = 'win')
+response = requests.get('https://spb.hh.ru/search/vacancy?text=python&area=1&area=2', headers = headers_gen.generate())
+html_data = response.text
+
+
+hh_main = BeautifulSoup(html_data, 'lxml')
+vacansy_list = hh_main.find('main', class_ = 'vacancy-serp-content')
+
+#print(vacansy_list)
+vacansy_tags = vacansy_list.find_all('div', class_="vacancy-serp-item-body__main-info")
+#print(vacansy_tags)
+
+vacansy_list = list()
+x = 0
+for vacansy_tag in vacansy_tags:
+    x+=1
+    if x <= 10:
+
+        salary = vacansy_tag.find('span', class_="bloko-header-section-2")
+        if salary == None:
+            salary_text = 'Заработная плата не указана'
+        else:
+            salary_text = salary.text
+            salary_text = unicodedata.normalize("NFKD",salary_text)
+        #print(salary_text)
+        header_tag = vacansy_tag.find('h3')
+        a_tag = header_tag.find('a')
+        header_text = header_tag.text
+        link = a_tag['href']
+        #print(link)
+        company = vacansy_tag.find('a', class_="bloko-link bloko-link_kind-tertiary")
+        company_text = company.text
+        company_text = unicodedata.normalize("NFKD",company_text)
+
+
+        vacansy_city = vacansy_tag.find('div', class_="vacancy-serp-item__info")
+        vacansy_city_1 = vacansy_city.find('div', class_="bloko-text")
+        vacansy_city_text = vacansy_city.text
+        if 'Москва' in vacansy_city_text:
+            vacansy_city_text = 'Москва'
+        elif 'Санкт-Петербург' in vacansy_city_text:
+            vacansy_city_text = 'Санкт-Петербург'
+        #print(vacansy_city_text)
+        vacansy_response = requests.get(link, headers = headers_gen.generate())
+        vacansy = BeautifulSoup(vacansy_response.text, 'lxml')
+        vacansy_body = vacansy.find('div', class_="g-user-content")
+        if vacansy_body == None:
+            vacansy_body_text = 'Нет описания вакансии'
+        else:
+            vacansy_boty_text = vacansy_body.text
+
+
+        if "Django" or "Flask" in vacansy_body_text:
+            vacansy_list.append({
+                'link': link,
+                'salary': salary_text,
+                'company': company_text,
+                'city': vacansy_city_text
+            })
+        pprint(vacansy_list)
+
+        with open('vacansy file.json', 'w', encoding='utf8') as f:
+
+            f.write(json.dumps(vacansy_list, ensure_ascii=False))
+    else:
+        break
+
+
